314c8b97929a50797ea0efcbb789a84ccfa808d4
camilo4-6 <juan.arroyave48@correo.tdea.edu.co>
2024-08-07 14:28:12 -0500

    se agrega cosas

--- src/clubproject/ClubProject.java
+++ src/clubproject/ClubProject.java
@@ -11,7 +11,8 @@ public class ClubProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here]
+        System.out.println("hola");
     }
     
 }
