fbd2aa852a75634c89bfa41ea1ca19ee95e58ded
Diover24 <diover.sanchez@correo.tdea.edu.co>
2024-08-18 23:41:17 -0500

    mano auida

--- src/clubproject/ClubProject.java
+++ src/clubproject/ClubProject.java
@@ -12,7 +12,7 @@ public class ClubProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here]
-        System.out.println("hola");
+        System.out.println("queso");
     }
     
 }
