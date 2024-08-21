f0e3135d3df3d7874b375eb653d03ed2abcc605c
Diover24 <diove.sanchez@correo.tdea.edu.co>
2024-08-21 16:54:38 -0500

    nuevo

---
+++ src/main/java/app/dto/PersonDto.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.dto;
+
+/**
+ *
+ * @author Camilo
+ */
+public class PersonDto {
+
+    private long id;
+    private long document;
+    private String name;
+    private long celPhone;
+
+    public PersonDto() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+  
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getDocument() {
+        return document;
+    }
+
+    public void setDocument(long document) {
+        this.document = document;
+    }
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCelPhone() {
+        return celPhone;
+    }
+
+    public void setCelPhone(long celPhone) {
+        this.celPhone = celPhone;
+    }
+
+}
---
+++ src/main/java/app/model/Person.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.model;
+
+public class Person {
+
+    private long id;
+    private long document;
+    private String name;
+    private long celPhone;
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getDocument() {
+        return document;
+    }
+
+    public void setDocument(long document) {
+        this.document = document;
+    }
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCelPhone() {
+        return celPhone;
+    }
+
+    public void setCelPhone(long celPhone) {
+        this.celPhone = celPhone;
+    }
+}
